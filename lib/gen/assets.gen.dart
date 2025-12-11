7c194e7638e6794e78b13e432b5106205ddefd31
malek <moustafasalemdeif@gmail.com>
2025-12-11 10:20:01 +0200

    using assets gen

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,174 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/Animals.png
+  AssetGenImage get animals => const AssetGenImage('assets/icons/Animals.png');
+
+  /// File path: assets/icons/Colors.png
+  AssetGenImage get colors => const AssetGenImage('assets/icons/Colors.png');
+
+  /// File path: assets/icons/Numbers.png
+  AssetGenImage get numbers => const AssetGenImage('assets/icons/Numbers.png');
+
+  /// File path: assets/icons/family Members.png
+  AssetGenImage get familyMembers =>
+      const AssetGenImage('assets/icons/family Members.png');
+
+  /// File path: assets/icons/interactive.png
+  AssetGenImage get interactive =>
+      const AssetGenImage('assets/icons/interactive.png');
+
+  /// File path: assets/icons/progress.png
+  AssetGenImage get progress =>
+      const AssetGenImage('assets/icons/progress.png');
+
+  /// File path: assets/icons/reward.png
+  AssetGenImage get reward => const AssetGenImage('assets/icons/reward.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    animals,
+    colors,
+    numbers,
+    familyMembers,
+    interactive,
+    progress,
+    reward,
+  ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/onboarding1.png
+  AssetGenImage get onboarding1 =>
+      const AssetGenImage('assets/images/onboarding1.png');
+
+  /// File path: assets/images/onboarding2.png
+  AssetGenImage get onboarding2 =>
+      const AssetGenImage('assets/images/onboarding2.png');
+
+  /// File path: assets/images/onboarding3.png
+  AssetGenImage get onboarding3 =>
+      const AssetGenImage('assets/images/onboarding3.png');
+
+  /// File path: assets/images/welcome screen.png
+  AssetGenImage get welcomeScreen =>
+      const AssetGenImage('assets/images/welcome screen.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    onboarding1,
+    onboarding2,
+    onboarding3,
+    welcomeScreen,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+    this.animation,
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+  final AssetGenImageAnimation? animation;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class AssetGenImageAnimation {
+  const AssetGenImageAnimation({
+    required this.isAnimation,
+    required this.duration,
+    required this.frames,
+  });
+
+  final bool isAnimation;
+  final Duration duration;
+  final int frames;
+}
