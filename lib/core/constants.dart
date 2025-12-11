a759619288873904b06ae72b53d2e38d7f9b96c0
malek <moustafasalemdeif@gmail.com>
2025-12-11 10:36:56 +0200

    add const list for title,dec

---
+++ lib/core/constants.dart
@@ -0,0 +1,11 @@
+const List<String> title = [
+  'Welcome to English Explorer!',
+  'Unlock the World of Languages',
+  'Ready to Speak a New Language?',
+];
+
+const List<String> description = [
+  'Embark on a fun-filled journey to master English with interactive exercises, quizzes, and exciting games!',
+  'Explore interactive lessons, track your progress, and earn rewards as you learn new languages.',
+  "Embark on your language learning adventure with interactive lessons and fun challenges. Let's get started!",
+];
